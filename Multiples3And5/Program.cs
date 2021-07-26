8f03dcebb75dfdba23e04be7701cc41fcc766a31
Ryan Palmer <rjpalmer9888@gmail.com>
2021-07-26 15:34:28 -0500

    Solved problem

--- Multiples3And5/Program.cs
+++ Multiples3And5/Program.cs
@@ -10,7 +10,16 @@ namespace Multiples3And5
     {
         static void Main(string[] args)
         {
+            int sum = 0; //Create a running total
+            for (int i = 1; i < 1000; i++) //Run a for loop that increments up to 100
+            {
+                if (i % 3 == 0 || i % 5 == 0) //If the number leaves a remainder of 0 when divided by 3 or 5, add it to the running total
+                {
+                    sum += i; //Running total equals the running total plus the number
+                }
+            }
 
+            Console.WriteLine("The sum of all numbers less than 1000 that are divisible by 3 or 5 is " + sum);
         }
     }
 }
